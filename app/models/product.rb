ad8681dd4fd7e152bf68a7c4e2db08e12287c1c2
Andy Weiss <andyweiss1982@gmail.com>
2014-08-22 13:58:13 -0400

    prepare for heroku

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,7 @@ class Product < ActiveRecord::Base
 		presence: true, 
 		numericality: {only_integer: true, greater_than: 0}
 	has_many :reviews
-	has_attached_file :avatar, :styles => { :medium => "300x300#", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :avatar, :styles => { :medium => "300x300#", :thumb => "100x100#" }
   	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 	def formatted_price
